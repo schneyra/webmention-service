ed6e34b656f24fe9070b35b37b7234b1a15249bc
Martin Schneider <mail@schneyra.de>
2021-02-19 10:56:11 +0100

    Update webmentions.js

--- netlify/functions/webmentions.js
+++ netlify/functions/webmentions.js
@@ -1,6 +1,78 @@
+async function getWebmentions(url) {
+    return new Promise((resolve, reject) => {
+        fetch(
+            `https://webmention.io/api/mentions.jf2?domain=martinschneider.me&per-page=200&sort-dir=up&target=${url}/`
+        )
+            .then((response) => response.json())
+            .then((data) => resolve(data));
+    });
+}
+
+function renderWebmentions(webmentions) {
+    let html = "";
+
+    if (webmentions.length) {
+        // now we're generating the actual list
+        html += `<ol class="webmentions">`;
+
+        webmentions.forEach((mention) => {
+            const twitterString = mention.url.includes("twitter.com")
+                ? " on twitter"
+                : "";
+
+            html += `<li class="webmentions__mention"><span class="webmentions__head">`;
+
+            if (mention.author.photo) {
+                html += `<img src="${mention.author.photo}" class="webmentions__avatar" loading="lazy" height="256" width="256" />`;
+            }
+
+            html += `<span>${
+                mention.author.name
+                    ? mention.author.name
+                    : "<em>Someone</em>"
+            } `;
+
+            if (mention["wm-property"] === "like-of") {
+                html += `liked <a href="${mention.url}">this post</a>${twitterString}.</span></span>`;
+            }
+
+            if (
+                mention["wm-property"] === "in-reply-to" &&
+                mention.content
+            ) {
+                html += `
+                replied to <a href="${mention.url}">this post</a>${twitterString}.</span></span>
+                <blockquote class="webmentions__quote">${mention.content.text}</blockquote>
+            `;
+            }
+
+            if (mention["wm-property"] === "repost-of") {
+                html += `reposted <a href="${mention.url}">this post</a>${twitterString}.</span></span>`;
+            }
+
+            if (mention["wm-property"] === "mention-of") {
+                html += `mentioned the article in <a href="${mention.url}">this post</a>${twitterString}.</span></span>`;
+            }
+
+            html += `</li>`;
+        });
+
+        html += `</ol>`;
+    } else {
+        html += "<p>No mentions yet. Be the first to share this post!</p>";
+    }
+
+    return html;
+}
+
 exports.handler = async function(event, context) {
+    const webmentions = await getWebmentions(event.queryStringParameters.url);
+
+
+    const html = renderWebmentionsHtml(webmentions.children);
+    
     return {
         statusCode: 200,
-        body: JSON.stringify({message: `${event.queryStringParameters.url}`})
+        body: JSON.stringify({message: html})
     };
 }
