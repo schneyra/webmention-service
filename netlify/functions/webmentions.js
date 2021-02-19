2f40dc369f7c6f8b234cc9754d1453c7cb7a0705
Martin Schneider <mail@schneyra.de>
2021-02-19 10:48:14 +0100

    Update webmentions.js

--- netlify/functions/webmentions.js
+++ netlify/functions/webmentions.js
@@ -1,6 +1,6 @@
 exports.handler = async function(event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify({message: `${event.path}`})
+        body: JSON.stringify({message: `${event.queryStringParameters}`})
     };
 }
